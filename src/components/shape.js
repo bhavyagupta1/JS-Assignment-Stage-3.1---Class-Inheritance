06d291c47fc52024c776fa7ae5620c46fa1027c6
bhavyagupta1 <48151042+bhavyagupta1@users.noreply.github.com>
2020-11-08 16:59:35 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,21 @@
 // declare class
 
 // export class using module.exports
+// declare class
+
+// export class using module.exports
+class Shape{
+    constructor(){
+        this.color="red";
+    }
+    drawShape(){
+    
+          console.log("draw a shape");
+    }
+    calculateArea(){
+         console.log("Dont want to calculate now");
+         return 0;
+    }
+}
+
+module.exports = Shape;
